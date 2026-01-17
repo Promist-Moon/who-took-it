0702a1d2add3d2286ed3c012a0fb61d69f64263e
Emilia Ayu <emiliaayu944@gmail.com>
2026-01-17 21:32:16 +0800

    Change interval length

--- who_took_it/detector/static/detector/camera.js
+++ who_took_it/detector/static/detector/camera.js
@@ -66,7 +66,7 @@ document.addEventListener("DOMContentLoaded", () => {
         // Later send frameBlob to Django / ML
         analyseFrame(frameBlob);
       });
-    }, 1000);
+    }, 2000);
 
   }
 
